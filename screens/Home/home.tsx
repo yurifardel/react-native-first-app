61af79c000d006a96382606c520fa0ed8b8bbadd
yuri fardel <yrnascimento@outlook.com>
2022-03-07 23:37:55 -0300

    fix: handle a data request

--- screens/Home/home.tsx
+++ screens/Home/home.tsx
@@ -16,43 +16,41 @@ import {
 import pokemon from "../../assets/images/pokemon.png";
 import axios from "axios";
 
-
-export type Props = { 
-  id: string,
-  name: string,
-  avatar: string
-}
-let list: Array<Props> = []
-
+export type Props = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+let list: Array<Props> = [];
 
 const Home = ({ Props }) => {
-  const [schema, setSchema] = useState(Props)
+  const [schema, setSchema] = useState(Props);
 
-  useEffect(() => {
-    const axiosReq = async () => {
-      try {
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
+  try {
+    (async () => {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/");
 
-        response.data["results"].map(
-          async (item: {
-            name: string;
-            url: string;
-          }) => {
-            const requestImageUrl = await axios.get(item.url);
-            const _id =  Math.random().toString(16).slice(2) + Date.now().toString(16).slice(4) 
+      response.data["results"].map(
+        async (item: { name: string; url: string }) => {
+          const requestImageUrl = await axios.get(item.url);
+          const _id =
+            Math.random().toString(16).slice(2) +
+            Date.now().toString(16).slice(4);
+          setSchema({
+            id: _id,
+            name: item.name,
+            avatar: requestImageUrl.data.sprites.front_default,
+          });
+        }
+      );
+    })();
+  } catch (e) {
+    console.log(e);
+  }
 
-            setSchema({id: _id, name: item.name, avatar: requestImageUrl.data.sprites.front_default})
-          }
-        );
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    axiosReq();
-  }, []);
+  
+    list.push(schema);
 
-
-  list.push(schema)
 
 
   return (
@@ -67,23 +65,24 @@ const Home = ({ Props }) => {
         </SearchContents>
         <Contents>
           <PokemonContainers>
-            {
-              list.map(items => {
-                return (
-                  <PokemonContents >
-                  <Avatar 
-                    key={items.id}
+            {list.map((items) => {
+              if(typeof items === 'undefined'){
+                return 
+              }
+              return (
+                <PokemonContents key={Math.random().toString(16).slice(2)}>
+                  <Avatar
+                    key={Math.random().toString(16).slice(2)}
                     source={{
                       uri: items.avatar,
                     }}
                   />
                   <BackgroundName>
-                    <PokemonName key={items.id} >{items.name}</PokemonName>
+                    <PokemonName key={Math.random().toString(16).slice(2)}>{items.name}</PokemonName>
                   </BackgroundName>
                 </PokemonContents>
-                )
-              })
-            }
+              );
+            })}
           </PokemonContainers>
         </Contents>
       </ScrollView>
